f845c2b8e97dac3fcbf223afc8fb97f61ce384d7
Bryn Stringer <kaos.nyrb@gmail.com>
2024-08-12 15:01:14 +0100

    Cloning worldspaces
    clones a worldspace, terrain block and file.
    makes new cells and content node

---
+++ ssf.models/Generation/NameGenerator.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ssf.Generation
+{
+    public class NameGenerator
+    {
+        public static List<string> adj = new List<string>
+        {
+            "Forsaken",
+            "Deserted",
+            "Neglected",
+            "Discarded",
+            "Forgotten",
+            "Abandoned",
+            "Relinquished",
+            "Vacant",
+            "Desolate",
+            "Unoccupied",
+            "Derelict",
+            "Despoiled",
+            "Isolated",
+            "Renounced",
+            "Surrendered",
+            "Disowned",
+            "Disregarded",
+            "Unclaimed",
+            "Ignored",
+            "Forsook",
+            "Vacated",
+            "Uninhabited",
+            "Unprotected",
+            "Destitute",
+            "Forgotten",
+            "Dilapidated",
+            "Ramshackle",
+            "Decrepit",
+            "Desolate",
+            "Decayed",
+        };
+
+        public static List<string> noun = new List<string>
+        {
+            "Warehouse",
+            "Storehouse",
+            "Storage",
+            "Station",
+            "Terminal",
+            "Store",
+            "Repository",
+            "Stockroom",
+            "Storage Facility",
+            "Supply Center",
+            "Distribution Center",
+            "Freight Station",
+            "Goods Yard",
+            "Stockpile",
+            "Storage Depot",
+            "Transit Point",
+            "Holding Area",
+            "Shipping Center",
+            "Supply Depot",
+            "Loading Dock",
+            "Processing Plant",
+            "Manufacturing Plant",
+            "Factory",
+            "Plant",
+            "Mill",
+            "Purification Plant",
+            "Smelting Plant",
+            "Extraction Plant",
+            "Industrial Facility",
+            "Processing Facility",
+            "Distillery",
+            "Chemical Plant",
+            "Production Facility",
+            "Refining Plant",
+            "Treatment Plant",
+            "Base",
+            "Garrison",
+            "Fort",
+            "Camp",
+            "Outpost",
+            "Barracks",
+            "Station",
+            "Encampment",
+            "Installation",
+            "Stronghold",
+            "Command Center",
+            "Military Base",
+            "Post",
+            "Cantonment",
+            "Headquarters",
+            "Defense Post",
+            "Army Post",
+            "Operational Base",
+            "Staging Area",
+            "Military Installation"
+        };
+
+
+        public static string GetRandomPOIName(int seed)
+        {
+            string result = "";
+            Random rand = new Random(seed);
+
+            result = adj[rand.Next(adj.Count - 1)] + " " + noun[rand.Next(noun.Count - 1)];
+
+            return result;
+        }
+    }
+}
--- ssf.models/POI/POIBuilder.cs
+++ ssf.models/POI/POIBuilder.cs
@@ -1,14 +1,17 @@
 ﻿using Mutagen.Bethesda;
 using Mutagen.Bethesda.Environments;
 using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Records;
 using Mutagen.Bethesda.Starfield;
 using Noggog;
+using ssf.Generation;
 using ssf.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ssf.POI
@@ -28,8 +31,8 @@ namespace ssf.POI
             string pluginname = settings.EspName;
             string datapath = "";
 
-            string prefix = settings.cellname;
-            string item = "suffix";
+            string prefix = "1to";
+
             StarfieldMod myMod;
 
             using (var env = GameEnvironment.Typical.Builder<IStarfieldMod, IStarfieldModGetter>(GameRelease.Starfield).Build())
@@ -55,135 +58,40 @@ namespace ssf.POI
                         }
                     }
                 }
+                //Location                
+                string poiname = NameGenerator.GetRandomPOIName(settings.seed);
+                string vowels = "aeiouy ";
+                string shortname = poiname.ToLower();
+                shortname = new string(shortname.Where(c => !vowels.Contains(c)).ToArray());
+                string item = shortname;
 
-                //What we need to build:
-                /*
-                //Packin Cell
-                Console.WriteLine("Building Record : " + prefix + "_cell_" + item);
-                var newCell = new Cell(myMod)
-                {
-                    EditorID = settings.cellname,
-                    Temporary = new ExtendedList<IPlaced>(),
-                    Flags = Cell.Flag.IsInteriorCell,
-                    Lighting = new CellLighting()
-                    {
-                        DirectionalFade = 1,
-                        FogPower = 1,
-                        FogMax = 1,
-                        NearHeightRange = 10000,
-                        Unknown1 = 1951,
-                        Unknown2 = 3,
-                    },
-                };
-                IFormLink<IPlaceableObjectGetter> PrefabPackinPivotDummy = new FormKey(env.LoadOrder[0].ModKey, 0x0003F808).ToLink<IPlaceableObjectGetter>();
-                newCell.Temporary.Add(new Mutagen.Bethesda.Starfield.PlacedObject(myMod)
-                {
-                    Base = PrefabPackinPivotDummy,
-                    Position = new P3Float(0, 0, 0),
-                    Rotation = new P3Float(0, 0, 0)
-                });
-                var key = newCell.FormKey.ID;
-                var stringkey = key.ToString();
-                var cellblockNumber = int.Parse(stringkey.Substring(stringkey.Length - 1));
-                var subBlockNumber = int.Parse(stringkey.Substring(stringkey.Length - 2, 1));
+                SSFEventLog.EventLogs.Enqueue(poiname);
+                SSFEventLog.EventLogs.Enqueue(shortname);
+                SSFEventLog.EventLogs.Enqueue(prefix + "wld" + item);
+                
+                IFormLinkNullable<IKeywordGetter> LocTypeDungeon = new FormKey(env.LoadOrder[0].ModKey, 0x000254BC).ToNullableLink<IKeywordGetter>();
+                IFormLinkNullable<IKeywordGetter> LocTypeClearable = new FormKey(env.LoadOrder[0].ModKey, 0x00064EDE).ToNullableLink<IKeywordGetter>();
+                IFormLinkNullable<IKeywordGetter> LocTypeOE_Keyword = new FormKey(env.LoadOrder[0].ModKey, 0x001A5468).ToNullableLink<IKeywordGetter>();
+                IFormLinkNullable<IKeywordGetter> LocEncSpacers_Exclusive = new FormKey(env.LoadOrder[0].ModKey, 0x00283585).ToNullableLink<IKeywordGetter>();
+                IFormLinkNullable<IKeywordGetter> LocTypeOverlay = new FormKey(env.LoadOrder[0].ModKey, 0x002CA99D).ToNullableLink<IKeywordGetter>();
 
-                SSFEventLog.EventLogs.Enqueue("Created Cell :" + key.ToString("x"));
-                //Try and use existing cellblocks and subblocks first.
-                CellBlock cellblock = null;
-                bool newCellBlock = false;
-                for (int i = 0; i < myMod.Cells.Count; i++)
-                {
-                    if (myMod.Cells[i].BlockNumber == cellblockNumber)
-                    {
-                        cellblock = myMod.Cells[i];
-                        break;
-                    }
-                }
-                if (cellblock == null)
-                {
-                    cellblock = new CellBlock
-                    {
-                        BlockNumber = cellblockNumber,
-                        GroupType = GroupTypeEnum.InteriorCellBlock,
-                        SubBlocks = new ExtendedList<CellSubBlock>()
-                    };
-                    newCellBlock = true;
-                }
-
-                bool addSubblock = true;
-                for (int i = 0; i < cellblock.SubBlocks.Count; i++)
-                {
-                    if (cellblock.SubBlocks[i].BlockNumber == subBlockNumber)
-                    {
-                        addSubblock = false;
-                    }
-                }
-                if (addSubblock)
-                {
-                    cellblock.SubBlocks.Add(new CellSubBlock()
-                    {
-                        BlockNumber = subBlockNumber,
-                        GroupType = GroupTypeEnum.InteriorCellSubBlock,
-                        Cells = new ExtendedList<Cell>()
-                    });
-                }               
-
-                //Add complete cell
-                bool addedCell = false;
-                for (int i = 0; i < cellblock.SubBlocks.Count && !addedCell; i++)
-                {
-                    if (cellblock.SubBlocks[i].BlockNumber == subBlockNumber)
-                    {
-                        cellblock.SubBlocks[i].Cells.Add(newCell);
-                        addedCell = true;
-                    }
-                }
-                if (newCellBlock)
-                {
-                    myMod.Cells.Add(cellblock);
-                }
-                //Packin
-                Console.WriteLine("Building Record : " + prefix + "_pkn_" + item);
-                IFormLink<ITransformGetter> link = new FormKey(env.LoadOrder[0].ModKey, 0x00050FAC).ToLink<ITransformGetter>();
-                byte[] barray = new byte[4] { 14, 00, 00, 00 };
-                var packin = new PackIn(myMod)
-                {
-                    MajorFlags = PackIn.MajorFlag.Prefab,
-                    EditorID = prefix + "_pkn_" + item,
-                    ObjectBounds = new ObjectBounds()
-                    {
-                        First = new P3Float(-4, -4, -1.767578f),
-                        Second = new P3Float(4, 4, 1.767578f)
-                    },
-                    ODTY = 0,
-                    Cell = newCell.ToNullableLink<ICellGetter>(),
-                    Version = 0,
-                    FNAM = new MemorySlice<byte>(barray),
-                    MaterialSwaps = new ExtendedList<IFormLinkGetter<ILayeredMaterialSwapGetter>>()
-                };
-                myMod.PackIns.Add(packin);
-                */
-                //Location
                 Location location = new Location(myMod)
                 {
-                    EditorID = prefix + "_loc_" + item,
-                    Name = item,
+                    EditorID = prefix + "loc" + item,
+                    Name = poiname,
                     Keywords = new ExtendedList<IFormLinkGetter<IKeywordGetter>>(),
                     WorldLocationRadius = 0,
                     ActorFadeMult = 1,
                     TNAM = 0,
                 };
 
-                myMod.Locations.Add(location);
+                location.Keywords.Add(LocTypeDungeon);
+                location.Keywords.Add(LocTypeClearable);
+                location.Keywords.Add(LocTypeOE_Keyword);
+                location.Keywords.Add(LocEncSpacers_Exclusive);
+                location.Keywords.Add(LocTypeOverlay);
 
-                //GalbankMarker - useful for debugging
-                IFormLinkNullable<IPlaceableObjectGetter> NA_Skyscraper_BLD_Galbank_01 = new FormKey(env.LoadOrder[0].ModKey, 0x002C76F3).ToNullableLink<IPlaceableObjectGetter>();
-                var placed = new Mutagen.Bethesda.Starfield.PlacedObject(myMod)
-                {
-                    Base = NA_Skyscraper_BLD_Galbank_01,
-                    StarfieldMajorRecordFlags = StarfieldMajorRecord.StarfieldMajorRecordFlag.VisibleWhenDistant,
-                    Position = new P3Float(),
-                };
+                myMod.Locations.Add(location);
 
                 //TopCell
                 var topcell = new Cell(myMod)
@@ -193,153 +101,74 @@ namespace ssf.POI
                     WaterHeight = 0,
                     XILS = 1,
                     MajorFlags = Cell.MajorFlag.Persistent,
-                    Persistent = new ExtendedList<IPlaced>()                    
+                    Persistent = new ExtendedList<IPlaced>()
                 };
-                topcell.Persistent.Add(placed);
 
-                //Worldspace
-                //WaterClear "Water" [WATR:00000018]
-                IFormLinkNullable<IWaterGetter> WaterClear = new FormKey(env.LoadOrder[0].ModKey, 0x00000018).ToNullableLink<IWaterGetter>();
-                byte[] FNAM = new byte[1] { 00 };
-                byte[] HNAM = new byte[1] { 05 };
-                Worldspace worldspace = new Worldspace(myMod)
+               
+                var baseworld = myMod.Worldspaces[new FormKey(myMod.ModKey, 0x00000CEB)];
+
+                var newworld = myMod.Worldspaces.DuplicateInAsNewRecord(baseworld);
+                
+                var newblock = myMod.SurfaceBlocks.DuplicateInAsNewRecord(myMod.SurfaceBlocks[new FormKey(myMod.ModKey, 0x00000CEC)]);
+
+                string newterrainfile = "Data\\Terrain\\" + prefix + "wld" + item + ".btd";
+                try
                 {
-                    EditorID = prefix + "_world_" + item,
-                    Components = new ExtendedList<AComponent>(),
-                    Name = prefix + "_world_" + item,
-                    Location = location.ToNullableLink<ILocationGetter>(),
-                    LodWater = WaterClear,
-                    LodWaterHeight = 0,
-                    LandDefaults = new WorldspaceLandDefaults() { DefaultLandHeight = -2048, DefaultWaterHeight = -200 },
-                    MapData = new WorldspaceMap()
-                    {
-                        UsableDimensions = new P2Int(0, 0),
-                        NorthwestCellCoords = new P2Int16(0, 0),
-                        SoutheastCellCoords = new P2Int16(0, 0),
-                    },
-                    WorldMapCellOffset = new P3Float(0, 0, 0),
-                    WorldMapOffsetScale = 1,
-                    DistantLodMultiplier = 1,
-                    Flags = Worldspace.Flag.SmallWorld,
-                    GNAM = 1,
-                    FNAM = FNAM,
-                    ObjectBoundsMin = new P2Float(-2, -2),
-                    ObjectBoundsMax = new P2Float(3, 3),
-                    HNAM = HNAM,
-                    SubCells = new ExtendedList<WorldspaceBlock>(),
-                    TopCell = topcell
-                };
-                byte[] PNAM = new byte[4] { 0xFF, 0xFF, 0xFF, 0xFF };
-                IFormLinkNullable<ISurfaceBlockGetter> OverlayBlockOEJM007World = new FormKey(env.LoadOrder[0].ModKey, 0x00098E69).ToNullableLink<ISurfaceBlockGetter>();
-                worldspace.Components.Add(new WorldSpaceOverlayComponent()
-                {
-                    PNAM = PNAM,
-                    SNAM = PNAM,
-                    SurfaceBlock = OverlayBlockOEJM007World
-                });
-
-                byte[] fourzero = new byte[4] { 0, 0, 0, 0 };
-                byte[] NAM4 = new byte[4] { 00, 0xFF, 00, 00 };
-                worldspace.Components.Add(new PlanetContentManagerContentPropertiesComponent()
-                {
-                    ZNAM = 0,
-                    YNAM = 0,
-                    XNAM = 0,
-                    WNAM = 0,
-                    VNAM = 1,
-                    UNAM = 0,
-                    NAM1 = 0,
-                    NAM3 = 0,
-                    NAM4 = NAM4,
-                    NAM5 = 0,
-                    NAM6 = 0,
-                    NAM7 = 0,
-                    NAM8 = 0,
-                    NAM9 = 0,
-                    Conditions = new ExtendedList<Condition>()
-                });
-
-
-                for (short x = 0; x < 2; x++)
-                {
-                    for(short y = 0; y < 2; y++)
-                    {
-                        var wsb = new WorldspaceBlock()
-                        {
-                            BlockNumberX = x,
-                            BlockNumberY = y,
-                            GroupType = GroupTypeEnum.ExteriorCellBlock,
-                            Items = new ExtendedList<WorldspaceSubBlock>(),                            
-                        };
-                        wsb.Items.Add(new WorldspaceSubBlock()
-                        {
-                            BlockNumberX = x,
-                            BlockNumberY = y,
-                            Items = new ExtendedList<Cell>(),
-                            GroupType = GroupTypeEnum.ExteriorCellSubBlock,                            
-                        });
-                        //Loop here?
-                        var newcell = new Cell(myMod)
-                        {
-                            Grid = new CellGrid() { Point = new P2Int(0, 0) },
-                            Flags = Cell.Flag.HasWater,
-                            XILS = 1,
-                            Temporary = new ExtendedList<IPlaced>()                            
-                        };
-
-
-//                        newcell.Temporary.Add(placed);
-
-                        wsb.Items[0].Items.Add(newcell);
-                        worldspace.SubCells.Add(wsb);
-                    }
+                    File.Copy(env.DataFolderPath + "\\Terrain\\stbblock001.btd", env.DataFolderPath + "\\Terrain\\" + prefix + "wld" + item + ".btd");
                 }
+                catch
+                {
+                    SSFEventLog.EventLogs.Enqueue("Terrain probs exists");
+                }
+                newblock.ANAM = newterrainfile;
+                newblock.EditorID = "OverlayBlock" + prefix + "wld" + item;
+                ((WorldSpaceOverlayComponent)newworld.Components[0]).SurfaceBlock = newblock.ToNullableLink<ISurfaceBlockGetter>();
 
-                myMod.Worldspaces.Add(worldspace);
+                newworld.EditorID = prefix + "wld" + item;
+                newworld.Location = location.ToNullableLink<ILocationGetter>();
+                newworld.Name = poiname;
+                newworld.TopCell = new Cell(myMod)
+                {
+                    Flags = Cell.Flag.HasWater,
+                    Grid = new CellGrid(),
+                    WaterHeight = -200,
+                    XILS = 1,
+                    MajorFlags = Cell.MajorFlag.Persistent,
+                    Persistent = new ExtendedList<IPlaced>()
+                };
+                int ia = 0;
+                foreach(var sbc in newworld.SubCells)
+                {
+                    var point = sbc.Items[0].Items[0].Grid.Point;
 
-                //Planet Content Manager Content Node
-                IFormLinkNullable<IGlobalGetter> PCM_CellLoadMinDensity_VeryClose = new FormKey(env.LoadOrder[0].ModKey, 0x002A4406).ToNullableLink<IGlobalGetter>();
+                    sbc.Items[0].Items[0] = new Cell(myMod)
+                    {
+                        EditorID = prefix+newworld.EditorID + "cell" + ia++,
+                        Grid = new CellGrid() { Point = point },
+                        Flags = Cell.Flag.HasWater,
+                        XILS = 1,
+                        Temporary = new ExtendedList<IPlaced>(),
+                        WaterHeight = -200,
+                        
+                    };
+                   
+                }
+                //Create Content node
+
+                //Add content node to the branch
+                Random rand = new Random();
                 var pcmcn = new PlanetContentManagerContentNode(myMod)
                 {
-                    EditorID = prefix + "_pcmcn_" + item,
-                    IOVR = false,
-                    Content = worldspace.ToNullableLink<IPlanetContentTargetGetter>()
+                    EditorID = item + rand.Next(100),
+                    Content = newworld.ToNullableLink<IPlanetContentTargetGetter>()
                 };
-
                 myMod.PlanetContentManagerContentNodes.Add(pcmcn);
-                //Planet Content Manager Branch Node
-                var pcmbn = new PlanetContentManagerBranchNode(myMod)
-                {
-                    EditorID = prefix + "pcmbn" + item,
-                    Components = new ExtendedList<AComponent>(),
-                    Conditions = new ExtendedList<Condition>(),
-                    Nodes = new ExtendedList<IFormLinkGetter<IPlanetNodeGetter>>(),
-                    NAM1 = 2,
-                    NAM2 = 0,
-                    NAM5 = false
-                };
-                pcmbn.Nodes.Add(pcmcn);
-                byte[] nam4 = new byte[4] { 00, 0xFF, 00, 00 };
-                pcmbn.Components.Add(new PlanetContentManagerContentPropertiesComponent()
-                {
-                    ZNAM = 0,
-                    YNAM = 1,
-                    XNAM = 0,
-                    WNAM = 0,
-                    VNAM = 0,
-                    UNAM = 0,
-                    NAM1 = 0,
-                    Global = PCM_CellLoadMinDensity_VeryClose,
-                    NAM3 = 0,
-                    NAM4 = nam4,
-                    NAM5 = 0,
-                    NAM6 = 0,
-                    NAM7 = 0,
-                    NAM8 = 0,
-                    NAM9 = 0,
-                    Conditions = new ExtendedList<Condition>(),
-                });
-                myMod.PlanetContentManagerBranchNodes.Add(pcmbn);
+                myMod.PlanetContentManagerBranchNodes[new FormKey(myMod.ModKey, 0x00000F66)].Nodes.Add(pcmcn.ToLinkGetter<IPlanetNodeGetter>());
+
+            }
+            foreach (var rec in myMod.EnumerateMajorRecords())
+            {
+                rec.IsCompressed = false;
             }
             myMod.WriteToBinary(datapath + "\\" + pluginname + ".esm");
             SSFEventLog.EventLogs.Enqueue("Export complete!");
